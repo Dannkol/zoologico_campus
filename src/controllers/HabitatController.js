2bd07faf9cb9773c931a9b3ade334448632e25ea
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-08-27 17:53:38 -0500

    feat: :sparkles: controlador para crear una habitat

--- src/controllers/HabitatController.js
+++ src/controllers/HabitatController.js
@@ -10,4 +10,15 @@ export class HabitatController {
       res.status(500).json({ error: 'Error al traer los datos' })
     }
   }
+
+  static async createHabitat (req, res) {
+    try {
+      const data = req.body
+      const result = await Habitat.createHabitat({ habitat: data })
+      res.status(200).json({ id: result, message: 'se registro correctamente' })
+    } catch (error) {
+      console.log('error en el controlador: ' + error.message)
+      res.status(500).json({ error: 'Error al crear la habitat' })
+    }
+  }
 }
