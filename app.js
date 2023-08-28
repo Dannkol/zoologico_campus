719405673269a3c3cf17609a1dde7f4ff4c1c281
IvanSanchezDev <ivancampus2023@gmail.com>
2023-08-28 08:37:04 -0500

    feat: :sparkles: implementacion routes-versioning para los endpoints de personal

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-import express from 'express';
-import dotenv from "dotenv";
+import express from 'express'
+import dotenv from 'dotenv'
+import { routes as routes_personal_js } from './src/routes/v1/personal.routes.js'
+import routesVersioning from 'express-routes-versioning'
+import configureApp from './src/config/configExpress.js'
 
+dotenv.config()
 
-import configureApp from './src/config/configExpress.js';
-
-dotenv.config();
-
-const app = express();
-
+const app = express()
+const version = routesVersioning()
 // Configurar la aplicación Express
-configureApp(app);
+configureApp(app)
 
-// Definir las rutas
+// Definir las rutasres
+app.use('/personal',
+  version({
+    '1.0.0': routes_personal_js
+  })
 
+)
 
 // Definir puertos de escuhca
 
 const HOST = JSON.parse(process.env.SERVER)
 
-const PORT = HOST.PORT || 8080;
+const PORT = HOST.PORT || 8080
 
 app.listen(PORT, async () => {
-    console.log(`Example app listening at http://${HOST.HOSTNAME}:${PORT}`);
-  });
+  console.log(`Example app listening at http://${HOST.HOSTNAME}:${PORT}`)
+})
