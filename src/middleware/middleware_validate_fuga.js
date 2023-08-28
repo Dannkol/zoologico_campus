929e2b9ddef291b6b330ad9608dde95957871917
Daniel Manosalva <manosalvasierra@gmail.com>
2023-08-28 16:53:05 -0500

    feat: :zap: express-validator para bajas por fuga
    se creo el express-validator para las bajas por fuga de animales
    
    dannkol

--- src/middleware/middleware_validate_fuga.js
+++ src/middleware/middleware_validate_fuga.js
@@ -81,7 +81,68 @@ const fugaAnimal = [
     body("detalles.fuga.estado")
         .notEmpty()
         .withMessage("El estado de la fuga es obligatorio")
-    
-];
+        .isNumeric()
+        .withMessage("el estado de la de la fuga debe ser un numero del 0 a 2")
+        .custom(value => {
+            if (!(value >= 0 && value < 3)) {
+                throw new Error("Estado de la fuga no valido");
+            }
+            return true;
+        }),
+    body("detalles.fuga.acta.fecha")
+        .notEmpty()
+        .withMessage("la fecha del acta es obligatorio")
+        .isISO8601()
+        .withMessage("Tipo de fecha no valida")
+        .customSanitizer(value => new Date(value)),
+    body("detalles.fuga.acta.responsables")
+        .notEmpty()
+        .withMessage("Es obligatorio los responsables")
+        .isArray()
+        .withMessage("Los responsables deben ser un array")
+        .custom(async value => {
+            if (value) {
+                const promises = [];
+                for (let i = 0; i < value.length; i++) {
+                    promises.push(
+                        findOne(value[i], "usuarios")
+                    );
+                }
+                const results = await Promise.all(promises);
+                // Verificar si todas las consultas a la base de datos fueron exitosas
+                results.forEach((responsable) => {
+                    if (!responsable) {
+                        throw new Error("ID de responsable no válido");
+                    }
+                });
+            }
+        })
+        .customSanitizer(value => {
+            try {
+                return value.map(responsable => new ObjectId(responsable))
+            } catch (error) {
+                return 'NULL'
+            }
+        }),
+    body("detalles.fuga.acta.documentacion.doc_reporte_autoridades")
+        .notEmpty()
+        .withMessage("los documentos de utoridades son obligator")
+        .isString()
+        .withMessage("Los documentos de utoridades deben ser un string"),
+    body("detalles.fuga.acta.documentacion.doc_legal_zoo")
+        .notEmpty()
+        .withMessage("los documentos legales del zoo son obligator")
+        .isString()
+        .withMessage("Los documentos legales del zoo ser un string"),
+    body("detalles.fuga.acta.documentacion.informe_opinion_publica")
+        .notEmpty()
+        .withMessage("los documentos de informacion a la opinion publica son obligator")
+        .isString()
+        .withMessage("Los documentos de informacion a la opinion publica ser un string"),
+    body("detalles.fuga.acta.observaciones")
+        .optional()
+        .isArray()
+        .withMessage("observaciones es un array"),
+]   
 
 export { fugaAnimal };
