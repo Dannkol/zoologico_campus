5df1dc9ef6c0b58956b36c59671284b8a9432269
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-08-28 20:18:22 -0500

    feat: :sparkles: implementacion de enruteo para  dar baja de personal e implementacion de la validacion

--- src/routes/v1/personal.routes.js
+++ src/routes/v1/personal.routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { PersonalController } from '../../controllers/PersonalController.js'
 import { validarPersonal } from '../../middleware/middleware_validate_personal.js'
+import { validarBajaPersonal } from '../../middleware/middlware_baja_personal.js'
 
 export const routes = Router()
 
 routes.get('/', PersonalController.getAllPersonal)
 routes.post('/contratarPersonal', validarPersonal, PersonalController.contratarPersonal)
+routes.post('/darBajaPersonal', validarBajaPersonal, PersonalController.bajaPersonal)
