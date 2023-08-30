8dbea6f7f354e2e1ec41d9d889aea86ddb7cb5e1
IvanSanchezDev <ivancampus2023@gmail.com>
2023-08-30 07:39:52 -0500

    feat: :sparkles: Middleware de verificacion de rol y permisos a los recursos

--- src/middleware/verifyRol.js
+++ src/middleware/verifyRol.js
@@ -5,17 +5,30 @@ import { config } from 'dotenv'
 
 config('../../')
 
-export const isAdmin = async (req, res, next) => {
+const verifyRole = (requiredRole) => async (req, res, next) => {
   try {
-    const connection = await mongoConn()
-    const nameDb = JSON.parse(process.env.DB)
-    const db = getDB(nameDb.ATLAS_DATABASE)
-    const collection = db.collection('usuario')
-    const { id } = req.user
-    const _id = new ObjectId(id)
-    const usuario = await collection.findOne({ _id })
-    console.log(usuario)
+    const connection = await mongoConn();
+    const nameDb = JSON.parse(process.env.DB);
+    const db = getDB(nameDb.ATLAS_DATABASE);
+    const collection = db.collection('usuario');
+    const { id } = req.user;
+    const _id = new ObjectId(id);
+    const usuario = await collection.findOne({ _id });
+
+    if (usuario.rol === requiredRole) {
+      console.log("tiene permiso");
+      next();
+    } else {
+      res.status(400).json({ status: 400, message: 'no tiene permisos para acceder al recurso' });
+    }
   } catch (error) {
-    console.log('errror middlearwe' + error.message)
+    console.log('error middleware: ' + error.message);
   }
+};
+
+export const rol={
+  verifyAdmin: verifyRole(4),
+  verifyEmpleado:verifyRole(2),
+  verifyInvestigador:verifyRole(3),
+  verifyVisitante:verifyRole(1)
 }
