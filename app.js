93fbbca436d871ed7fce75a5131a0c12de5e2ab6
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-08-29 23:16:04 -0500

    feat: :sparkles: Implementacion de enruteo para endpoints relacionados a auth

--- app.js
+++ app.js
@@ -5,20 +5,22 @@ import configureApp from './src/config/configExpress.js'
 import { routes as routes_habitat_js } from './src/routes/v1/habitat.routes.js'
 import { routes as routes_personal_js } from './src/routes/v1/personal.routes.js'
 
-import { routes as routes_animal_v1 } from "./src/routes/v1/animal.routes.js";
+import { routes as routes_animal_v1 } from './src/routes/v1/animal.routes.js'
 import { routes as routes_visitas_v1 } from './src/routes/v1/visitas.routes.js'
+
+import { routes as routes_auth_v1 } from './src/routes/v1/auth.routes.js'
 
 // Rutas version 1
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
-const version = routesVersioning();
+const version = routesVersioning()
 // Configurar la aplicación Express
 configureApp(app)
-configureApp(app);
-app.use(express.urlencoded({ extended: true }));
+configureApp(app)
+app.use(express.urlencoded({ extended: true }))
 
 // Definir las rutasres
 app.use('/personal',
@@ -33,24 +35,29 @@ app.use('/habitat',
 
 )
 app.use('/animal',
-version({
-      "1.0.0": routes_animal_v1,
-    })
-);
+  version({
+    '1.0.0': routes_animal_v1
+  })
+)
 
 app.use('/visitas',
-version({
-      "1.0.0": routes_visitas_v1,
-    })
-);
+  version({
+    '1.0.0': routes_visitas_v1
+  })
+)
 
+app.use('/auth',
+  version({
+    '1.0.0': routes_auth_v1
+  })
+)
 
 // Definir puertos de escuhca
 
-const HOST = JSON.parse(process.env.SERVER);
+const HOST = JSON.parse(process.env.SERVER)
 
 const PORT = HOST.PORT || 8080
 
 app.listen(PORT, async () => {
-  console.log(`Example app listening at http://${HOST.HOSTNAME}:${PORT}`);
-});
+  console.log(`Example app listening at http://${HOST.HOSTNAME}:${PORT}`)
+})
